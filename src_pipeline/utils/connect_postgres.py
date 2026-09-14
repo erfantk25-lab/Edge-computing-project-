a4baec54ae03dae26290da320da71e0ca6fb8441
Josefin Lesley <josefin3647@gmail.com>
2026-09-14 16:27:42 +0200

    Added a function to query the database

--- src_pipeline/utils/connect_postgres.py
+++ src_pipeline/utils/connect_postgres.py
@@ -10,3 +10,9 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 HOST = os.getenv("DB_HOST", "localhost")
 
 DB_CONFIG = f"dbname={POSTGRES_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD} host={HOST}"
+
+def query_db(sql, parameters: tuple=None):
+    with psycopg2.connect(DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, parameters)
+            return cur.fetchall() if cur.description else None
