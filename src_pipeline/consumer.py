087363fb789b32a3e48214a4a2b7e690b9256d4f
erfantk25-lab <erfantk.25@gmail.com>
2026-09-14 20:03:15 +0200

    Add light sensor data to consumer and database

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 import json
-from utils.connect_postgres import query_db 
+from utils.connect_postgres import query_db
 
 
 def on_message(client, userdata, message):
@@ -9,30 +9,36 @@ def on_message(client, userdata, message):
 
     temperature = float(data["temperature"])
     humidity = float(data["humidity"])
+    light = float(data["light"])
 
     query_db(
         """
-        INSERT INTO sensor_readings 
-            (time, temperature, humidity) 
-        VALUES (NOW(), %s, %s)
-""",
-        (temperature, humidity),
+        INSERT INTO sensor_readings
+            (time, temperature, humidity, light)
+        VALUES (NOW(), %s, %s, %s)
+        """,
+        (temperature, humidity, light),
     )
 
-    print(temperature, humidity)
+    print(temperature, humidity, light)
 
 
 if __name__ == "__main__":
-    query_db("""
+    query_db(
+        """
         CREATE TABLE IF NOT EXISTS sensor_readings (
             time TIMESTAMPTZ NOT NULL,
             temperature DOUBLE PRECISION,
-            humidity DOUBLE PRECISION
+            humidity DOUBLE PRECISION,
+            light DOUBLE PRECISION
         )
-    """)
+        """
+    )
 
     client = mqtt.Client()
     client.connect("mosquitto", 1883)
+
     client.subscribe("home/pico/dht11")
+
     client.on_message = on_message
     client.loop_forever()
