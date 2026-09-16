10827f0ef64b793fc36b7c95aff44a8d75c420a0
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-09-16 10:41:13 +0200

    Fixed from the comments

--- src_pico/wifi.py
+++ src_pico/wifi.py
@@ -6,20 +6,20 @@ import time
 rp2.country("SE")
 
 with open("wifi_credentials.json") as file:
-    credentrials = json.load(file)
+    credentials = json.load(file)
 
 def connect_wifi(waiting_time = 10):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(False)
     time.sleep(1)
     wlan.active(True)
-    wlan.connect(credentrials.get("WIFI_SSID"), credentrials.get("WIFI_PASSWORD"))
+    wlan.connect(credentials.get("WIFI_SSID"), credentials.get("WIFI_PASSWORD"))
 
     print(f"{wlan.ifconfig()}")
 
     while waiting_time > 0:
       if wlan.isconnected():
-        print("Ansluten:", wlan.ifconfig())
+        print("Connected:", wlan.ifconfig())
         break
       print("status:", wlan.status())
       waiting_time -= 1
