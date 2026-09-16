1ced8b5c132efc3840481725397ac58d9a6cfdc9
Josefin Lesley <josefin3647@gmail.com>
2026-09-16 15:12:35 +0200

    Added function on_connect to consumer.py

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -2,30 +2,48 @@ import paho.mqtt.client as mqtt
 import json
 from utils.connect_postgres import query_db
 
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code == 0:    # 0 = succeeded to connect
+        print("Connected to broker")
+        client.subscribe("home/pico/dht11")
+        client.subscribe("home/pico/lux")
+    else:
+        print("Connection failed:", reason_code)
 
 def on_message(client, userdata, message):
+    """
+    Handles an incoming MQTT message by decoding JSON. Stores 
+    the information in the database, based on its topics. 
+    
+    """
+    
     payload = message.payload.decode()
     data = json.loads(payload)
 
-    temperature = float(data["temperature"])
-    humidity = float(data["humidity"])
-    lux = float(data["lux"])
+    if message.topic == "home/pico/dht11":
+        temperature = float(data["temperature"])
+        humidity = float(data["humidity"])
 
-    # TODO: Add light sensor data when the light sensor is implemented.
-    # light = float(data["light"])
+        query_db(
+                """
+                INSERT INTO sensor_readings
+                    (time, temperature, humidity)
+                VALUES (NOW(), %s, %s)
+                """,
+                (temperature, humidity),
+            )
 
-    query_db(
-        """
-        INSERT INTO sensor_readings
-            (time, temperature, humidity, lux)
-        VALUES (NOW(), %s, %s)
-        """,
-        (temperature, humidity),
-    )
+    elif message.topic == "home/pico/lux":
+        lux = float(data["lux"])
 
-    print("Temperature:", temperature)
-    print("Humidity:", humidity)
-    print("Lux:", lux)
+        query_db(
+            """
+            INSERT INTO sensor_readings (time, lux)
+            VALUES (NOW(), %s)
+            """,
+            (lux,),
+        )
+        print("Lux:", lux)
 
 
 if __name__ == "__main__":
@@ -41,12 +59,8 @@ if __name__ == "__main__":
     )
 
     client = mqtt.Client()
-
-    client.connect("mosquitto", 1883)
-
-    client.subscribe("home/pico/dht11")
-    client.subscribe("home/pico/lux")
-
+    client.on_connect = on_connect
     client.on_message = on_message
 
+    client.connect("mosquitto", 1883)
     client.loop_forever()
